5968b9dff5f83c7708a8c8ca8a941a6ebf0c9e6d
kschilli <kschilli@ams-kschilling2.MSC.local>
2013-08-14 12:54:44 -0500

    remove method and update javadoc
    -remove gatherFileContents() method as it is now replaced with to
    gatherSMSFromFile() which uses createSMS() as a helper
    - Added some javadoc

--- src/merger/Read.java
+++ src/merger/Read.java
@@ -1,11 +1,8 @@
 package merger;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import javax.xml.stream.XMLEventReader;
@@ -21,12 +18,10 @@ public class Read {
 	private static final XMLInputFactory XMLINFACTORY = XMLInputFactory.newInstance();
 
 	/**
-	 * Compare file contents of 2 xml sms files
+	 * Gather and compare contents of xml sms files
 	 * 
-	 * @param firstFile
-	 *            First xml sms file to compare
-	 * @param secondFile
-	 *            Second xml sms file to compare
+	 * @param listOfFiles
+	 *            list of xml files to be compared
 	 */
 	public void compare(File[] listOfFiles) {
 		if (!(listOfFiles.length > 0)) return; // Files must exist
@@ -36,26 +31,28 @@ public class Read {
 		}
 
 		//TODO info gathered might need to be displayed in a JDialog instead of system console
+		//TESTING CODE
 		ArrayList<SMS> list = new ArrayList<SMS>();
 		for (File f : listOfFiles) {
-			list = createSmsNodes(f);
+			list = gatherSMSFromFile(f);
 		}
 
 		for (SMS sms : list) {
-			System.out.println(sms);
+			System.out.println(sms.toFileString());
 		}
-		
+		//ENDTESTINGCODE
+
 	}
 
 	/**
-	 * Take xml file and parse all the sms nodes in it into a sms object and
-	 * store them in an arraylist
+	 * Helper Method - Take xml file and parse all the sms nodes in it into a
+	 * sms object and store them in an arraylist
 	 * 
 	 * @param xmlFile
 	 *            passed in xml file
 	 * @return list of sms nodes
 	 */
-	private ArrayList<SMS> createSmsNodes(File xmlFile) {
+	private ArrayList<SMS> gatherSMSFromFile(File xmlFile) {
 		ArrayList<SMS> smsList = new ArrayList<SMS>();
 		try {
 			XMLEventReader eventReader = XMLINFACTORY.createXMLEventReader(new FileInputStream(xmlFile));
@@ -66,58 +63,7 @@ public class Read {
 					StartElement startElement = event.asStartElement();
 					if (startElement.getName().getLocalPart() == "sms") { // Get sms node
 						SMS sms = new SMS();
-						Iterator<?> attributes = startElement.getAttributes();
-						while (attributes.hasNext()) {
-							Attribute attribute = (Attribute) attributes.next();
-							System.out.println(attribute.getName().toString() + " --> " + attribute.getValue());
-							switch (attribute.getName().toString()) {
-							case "protocol":
-								sms.setProtocol(attribute.getValue());
-								break;
-							case "address":
-								sms.setAddress(attribute.getValue());
-								break;
-							case "date":
-								sms.setDate(attribute.getValue());
-								break;
-							case "type":
-								sms.setType(attribute.getValue());
-								break;
-							case "subject":
-								sms.setSubject(attribute.getValue());
-								break;
-							case "body":
-								sms.setBody(attribute.getValue());
-								break;
-							case "toa":
-								sms.setToa(attribute.getValue());
-								break;
-							case "sc_toa":
-								sms.setSc_toa(attribute.getValue());
-								break;
-							case "service_center":
-								sms.setService_Center(attribute.getValue());
-								break;
-							case "read":
-								sms.setRead(attribute.getValue());
-								break;
-							case "status":
-								sms.setStatus(attribute.getValue());
-								break;
-							case "locked": // Not always included
-								sms.setLocked(attribute.getValue());
-								break;
-							case "date_sent": // Not always included
-								sms.setDate_Sent(attribute.getValue());
-								break;
-							case "readable_date": // Optional
-								sms.setReadable_Date(attribute.getValue());
-								break;
-							case "contact_name": // Optional
-								sms.setContact_Name(attribute.getValue());
-								break;
-							}
-						}
+						sms = createSMS(startElement.getAttributes());
 						smsList.add(sms);
 					}
 				}
@@ -136,38 +82,65 @@ public class Read {
 	}
 
 	/**
-	 * Helper for compare method to collect all sms elements in a file and store
-	 * them in an arraylist
+	 * Helper method - Creates a new sms with given content attributes
 	 * 
-	 * @param file
-	 *            File that all sms elements will be gathered from
-	 * @return An arraylist containing the whole sms string as a full string
+	 * @return new sms node
 	 */
-	public ArrayList<String> gatherFileContents(File file) {
-		ArrayList<String> fileContent = new ArrayList<String>();
+	private SMS createSMS(Iterator<?> attributes) {
+		SMS sms = new SMS();
 
-		try {
-			String line = "";
-			BufferedReader reader = new BufferedReader(new FileReader(file));
-			reader.readLine(); // Skip XML Version
-			reader.readLine(); // Skip XML Style Sheet
-			line = reader.readLine(); // Skip SMS Count
-
-			while (!(line = reader.readLine()).contains("</smses>")) {
-				fileContent.add(line);
+		while (attributes.hasNext()) {
+			Attribute attribute = (Attribute) attributes.next();
+			switch (attribute.getName().toString()) {
+			case "protocol":
+				sms.setProtocol(attribute.getValue());
+				break;
+			case "address":
+				sms.setAddress(attribute.getValue());
+				break;
+			case "date":
+				sms.setDate(attribute.getValue());
+				break;
+			case "type":
+				sms.setType(attribute.getValue());
+				break;
+			case "subject":
+				sms.setSubject(attribute.getValue());
+				break;
+			case "body":
+				sms.setBody(attribute.getValue());
+				break;
+			case "toa":
+				sms.setToa(attribute.getValue());
+				break;
+			case "sc_toa":
+				sms.setSc_toa(attribute.getValue());
+				break;
+			case "service_center":
+				sms.setService_Center(attribute.getValue());
+				break;
+			case "read":
+				sms.setRead(attribute.getValue());
+				break;
+			case "status":
+				sms.setStatus(attribute.getValue());
+				break;
+			case "locked": // Not always included
+				sms.setLocked(attribute.getValue());
+				break;
+			case "date_sent": // Not always included
+				sms.setDate_Sent(attribute.getValue());
+				break;
+			case "readable_date": // Optional
+				sms.setReadable_Date(attribute.getValue());
+				break;
+			case "contact_name": // Optional
+				sms.setContact_Name(attribute.getValue());
+				break;
 			}
-			reader.close();
-
-		} catch (FileNotFoundException e) {
-			System.err.println("ERROR: Could not find file being passed in!");
-			System.err.println("ERROR MESSAGE: " + e.getMessage());
-			e.printStackTrace();
-		} catch (IOException e) {
-			System.err.println("ERROR: Could not read from file input stream!");
-			System.err.println("ERROR MESSAGE: " + e.getMessage());
-			e.printStackTrace();
 		}
 
-		return fileContent;
+		return sms;
 	}
+
 }
