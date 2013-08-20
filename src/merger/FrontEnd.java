89039bb7e3011c7b867e57ffe98d7ac4ac96b59d
schilkyl <schilkyl@gmail.com>
2013-08-20 15:22:04 -0500

    base features for creating sms
    - gets the basic details needed to create a new sms
    - readwrite will implement the rest

--- src/merger/FrontEnd.java
+++ src/merger/FrontEnd.java
@@ -4,23 +4,22 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.text.ParseException;
 import java.util.Arrays;
 import javax.swing.JButton;
-import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 
 public class FrontEnd {
 
+	private static final JFrame FRAME = new JFrame();
+	private static final ReadWrite READ_WRITE = new ReadWrite();
 	private static final JFileChooser CHOOSER = new JFileChooser();
 	private static final boolean DEBUG = true;
-	private static final ReadWrite READ_WRITE = new ReadWrite();
-	private static final JFrame FRAME = new JFrame();
 	private JButton selectFile, info, merge, extract, create;
 	private File[] files;
 
@@ -115,22 +114,116 @@ public class FrontEnd {
 		create.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JDialog dialog = new JDialog(FRAME);
-				dialog.setLayout(new GridLayout(5, 1));
+				int inInt;
+				String inString;
+				String address = "";
+				String date = "";
+				String type = "";
+				String body = "";
+				String read = "";
 
-				JTextField address = new JTextField("address");
-				JTextField date = new JTextField("date");
-				JTextField type = new JTextField("type");
-				JTextField body = new JTextField("body");
-				JTextField read = new JTextField("read");
+				// Type
+				inInt = JOptionPane.showConfirmDialog(create, "Is this an outgoing message?", "Message type",
+						JOptionPane.YES_NO_OPTION);
+				if (inInt == -1) return;
+				type = (inInt == 0) ? "2" : "1";
 
-				dialog.add(address);
-				dialog.add(date);
-				dialog.add(type);
-				dialog.add(body);
-				dialog.add(read);
+				if (inInt == 1) {
+					// Read
+					inInt = JOptionPane.showConfirmDialog(create, "Is this message read?", "Message read",
+							JOptionPane.YES_NO_OPTION);
+					if (inInt == -1) return;
+					read = (inInt == 0) ? "1" : "0";
+				}
 
-				//createMessage();
+				// Address
+				do {
+					inString = JOptionPane.showInputDialog(create, "Enter # to or from:", "1234567890");
+					if (inString == null) return;
+				} while (!inString.matches("\\d{10}"));
+				address = inString;
+
+				// Body
+				do {
+					inString = JOptionPane.showInputDialog(create, "Message content:", "");
+					if (inString == null) return;
+				} while (inString.length() == 0);
+				body = inString;
+
+				// Date
+				// Get month
+				String epochDate = "";
+				String[] months = { "January", "February", "March", "April", "May", "June", "July", "August",
+						"September", "October", "November", "December" };
+				inString = (String) JOptionPane.showInputDialog(create, "What month?", "Choose month",
+						JOptionPane.QUESTION_MESSAGE, null, months, months[0]);
+				if (inString == null) return;
+				epochDate += inString + " ";
+
+				// Get day
+				String[] days = { "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14",
+						"15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30",
+						"31" };
+				inString = (String) JOptionPane.showInputDialog(create, "On what day?", "Choose day",
+						JOptionPane.QUESTION_MESSAGE, null, days, days[0]);
+				if (inString == null) return;
+				epochDate += inString + " ";
+
+				// Get year
+				String[] years = { "1970", "1970", "1972", "1973", "1974", "1975", "1976", "1977", "1978", "1979",
+						"1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991",
+						"1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003",
+						"2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013" };
+				inString = (String) JOptionPane.showInputDialog(create, "Of what year?", "Choose year",
+						JOptionPane.QUESTION_MESSAGE, null, years, years[years.length - 1]);
+				if (inString == null) return;
+				epochDate += inString + " ";
+
+				// Get hours
+				String[] hours = { "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13",
+						"14", "15", "16", "17", "18", "19", "20", "21", "22", "23" };
+				inString = (String) JOptionPane.showInputDialog(create, "During what hour?", "Choose hour",
+						JOptionPane.QUESTION_MESSAGE, null, hours, hours[0]);
+				if (inString == null) return;
+				epochDate += inString + " ";
+
+				// Get minutes
+				String[] minutes = { "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12",
+						"13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28",
+						"29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44",
+						"45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59" };
+				inString = (String) JOptionPane.showInputDialog(create, "Within what minute?", "Choose minute",
+						JOptionPane.QUESTION_MESSAGE, null, minutes, minutes[0]);
+				if (inString == null) return;
+				epochDate += inString + " ";
+
+				// Get seconds
+				String[] seconds = { "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12",
+						"13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28",
+						"29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44",
+						"45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59" };
+				inString = (String) JOptionPane.showInputDialog(create, "At what second?", "Choose second",
+						JOptionPane.QUESTION_MESSAGE, null, seconds, seconds[0]);
+				if (inString == null) return;
+				epochDate += inString;
+
+				System.out.println(epochDate);
+				long epoch = 0;
+				try {
+					epoch = new java.text.SimpleDateFormat("MMM dd yyyy HH mm ss").parse(epochDate).getTime();
+				} catch (ParseException e1) {
+					e1.printStackTrace();
+				}
+				date = String.valueOf(epoch);
+
+				if (DEBUG) System.out.println("DEBUG: address: " + address + " date: " + date + " type: " + type
+						+ " body: " + body + " read: " + read);
+				createMessage(address, date, type, body, read);
+				//address – phone number of the sender/recipient
+				//date – Java date representation of the time when the message was sent/received
+				//type – 1 = Received, 2 = Sent, 3 = Draft, 4 = Outbox, 5 = Failed, 6 = Queued 
+				//body – content of the message
+				//read – Read Message = 1, Unread Message = 0
 			}
 		});
 	}
@@ -145,7 +238,7 @@ public class FrontEnd {
 		info.setEnabled(false);
 		merge.setEnabled(false);
 		extract.setEnabled(false);
-		create.setEnabled(false);
+		//create.setEnabled(false);
 
 		FRAME.setTitle(System.getProperty("user.name") + " :: " + getClass().getName().substring(0, 6));
 		FRAME.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
